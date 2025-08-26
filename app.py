3116af1de6d49917a718cd8012de8d5966d0ee13
Chanura04 <chanurakarunanayake12@gmail.com>
2025-08-27 02:24:49 +0530

    create tweets posting page and get user api data page

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 #request use for get and post request in database
 #redirect for different pages
 #generate_password_hash,check_password_hash password encrypting and decrypting
-import os
+import os,glob
 
 
-from flask import Flask,render_template,request,redirect,session,url_for
+from flask import Flask,render_template,request,redirect,session,url_for,flash
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 from celery import Celery
@@ -15,8 +15,13 @@ import tweepy
 app = Flask(__name__)
 app.secret_key="123"
 
+app.config["APP_STARTED"] = True
 
+def clear_session_files():
+    for file in glob.glob("flask_session/*"):
+        os.remove(file)
 
+clear_session_files()
 
 
 #configure sql alchemy
@@ -41,6 +46,10 @@ class User(db.Model):
     twitter_access_token = db.Column(db.String(100))
     twitter_access_token_secret = db.Column(db.String(100))
     screen_name = db.Column(db.String(100))
+    client_id=db.Column(db.String(100))
+    client_secret=db.Column(db.String(100))
+    isFilledApiDetails=db.Column(db.Boolean,default=False)
+    canPost=db.Column(db.Boolean,default=False)
 
     def set_password(self,password):
         self.password = generate_password_hash(password)
@@ -50,75 +59,12 @@ class User(db.Model):
 
 
 
-@app.route("/",methods=["GET","POST"])
+@app.route("/")
 def dashboard():
-    if request.method=="POST":
-        # redirect(url_for("connect_twitter"))
-
-
-        return render_template("get_api_details.html",  username=session.get('username'))
-
-    if request.method=="GET":
-        return redirect(url_for("login"))
-
-
-
-
-
-
-# ... (rest of your imports and code)
-
-@app.route("/post_tweet", methods=["POST"])
-def post_tweet():
-    """
-    Handles a form submission to post a new tweet.
-    """
-    tweet_text = request.form.get("tweet_content")
-    if not tweet_text:
-        # Handle case where tweet content is missing
-        return "Tweet content is required", 400
-
-    user = User.query.filter_by(email=session['email']).first()
-    if not user or not user.twitter_access_token:
-        # Redirect to the connect page if user is not authenticated
-        return redirect(url_for("connect_twitter"))
-
-    # Use the v2 client for posting tweets on the free tier
-    client = tweepy.Client(
-        consumer_key=user.twitter_api_key,
-        consumer_secret=user.twitter_api_secret,
-        access_token=user.twitter_access_token,
-        access_token_secret=user.twitter_access_token_secret
-    )
-
-    try:
-        response = client.create_tweet(text=tweet_text)
-        print("✅ Tweet posted successfully:", response.data)
-        # You could add a success flash message here
-    except Exception as e:
-        print("❌ Error posting tweet:", e)
-        # Add an error flash message here
-
-    return redirect(url_for("dashboard"))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    if session.get("username") and session.get("toDashboard"):
+        return render_template("dashboard.html", username=session.get('username'))
+    else:
+        return render_template("dashboard.html")
 
 
 
@@ -160,6 +106,7 @@ def login():
             if user.check_password(password):
                 print(user.FirstName)
                 session['username'] = user.FirstName
+                session['toDashboard'] = True
                 return redirect(url_for("dashboard"))
             else:
                 return render_template("login.html", error="Invalid Password...Please try again!",email=session[email])
@@ -176,58 +123,168 @@ def login():
 def signup():
     if request.method == "POST":
         email = request.form["email"]
+        if email:
+            new_user =User.query.filter_by(email=email).first()
+            if new_user:
+                return render_template("login.html",error="username already exists here!")
 
-        new_user =User.query.filter_by(email=email).first()
-        if new_user:
-            return render_template("login.html",error="username already exists here!")
+            else:
 
-        else:
+                first_name = request.form.get("first_name")
+                last_name = request.form.get("last_name")
+                email = request.form.get("email")
+                password = request.form.get("password")
+                print("📌 DEBUG Signup values:", first_name, last_name, email, password)
 
-            first_name = request.form.get("first_name")
-            last_name = request.form.get("last_name")
-            email = request.form.get("email")
-            password = request.form.get("password")
-            print("📌 DEBUG Signup values:", first_name, last_name, email, password)
-
-            new_user = User(FirstName=first_name,LastName=last_name,email=email)
-            new_user.set_password(password)
-            db.session.add(new_user)
-            db.session.commit()
-            session["username"] = first_name
-            return redirect(url_for("login"))
+                new_user = User(FirstName=first_name,LastName=last_name,email=email)
+                new_user.set_password(password)
+                db.session.add(new_user)
+                db.session.commit()
+                session["username"] = first_name
+                return redirect(url_for("login"))
     if request.method == "GET":
         return render_template("signup.html")
 
+# @app.route("/get_api_details" )
+# def get_api_details():
+#
+#         return render_template("get_api_details.html")
 
+# client = tweepy.Client()
+                    # client_id = request.args.get("client_id")
+                    # client_secret = request.args.get("client_secret")
+                    # user_access_token = request.args.get("user_access_token")
+                    # user_access_token_secret = request.args.get("user_access_token_secret")
 
-@app.route("/connect_twitter", methods=["POST"])
-def connect_twitter():
+def check_api_details():
     user = User.query.filter_by(email=session['email']).first()
-    user.twitter_api_key = request.form.get("api_key")
-    user.twitter_api_secret = request.form.get("api_secret")
-    user.twitter_access_token = request.form.get("access_token")
-    user.twitter_access_token_secret = request.form.get("access_token_secret")
-    user.screen_name = request.form.get("screen_name")
-    db.session.commit()
-    # tweets = get_user_tweets()
-    # return redirect(url_for("dashboard"))
-    return render_template("dashboard.html" )
+    if user:
+        client_id = user.client_id
+        client_secret = user.client_secret
+        user_access_token = user.twitter_access_token
+        user_access_token_secret = user.twitter_access_token_secret
+        if not client_id or not client_secret or not user_access_token or not user_access_token_secret:
+            return False
+        else:
+            return True
+    return False
+
+@app.route("/post_tweet",methods=["POST","GET"])
+def post_tweet():
+    if request.method == "POST":
+        if check_api_details():
+            user = User.query.filter_by(email=session['email']).first()
+
+            if user.canPost:
+                user = User.query.filter_by(email=session['email']).first()
+                if user:
+                    client_id = user.client_id
+                    client_secret = user.client_secret
+                    user_access_token = user.twitter_access_token
+                    user_access_token_secret = user.twitter_access_token_secret
+                    if not client_id or not client_secret or not user_access_token or not user_access_token_secret:
+
+                        client = tweepy.Client(
+                            consumer_key=client_id,
+                            consumer_secret=client_secret,
+                            access_token=user_access_token,
+                            access_token_secret=user_access_token_secret
+                        )
+                        texts = request.form.get("tweet_content")
+                        # Post a tweet
+                        try:
+                            response = client.create_tweet(text=texts)
+                            flash("🎉 Tweet posted successfully!")
+                            print(response)
+                        except Exception as e:
+                            flash("Error posting tweet!!!. Invalid API details...")
+                            print(f"Error posting tweet: {e}")
+                    else:
+                        flash("Please fill the api details!")
+                else:
+                    flash("Please login first!")
+        else:
+            return redirect(url_for("connect_twitter"))
+
+    if request.method == "GET":
+        return render_template("post_content.html")
 
 
 
 
+@app.route("/connect_twitter", methods=["POST","GET"])
+def connect_twitter():
+    if request.method == "POST":
+        user = User.query.filter_by(email=session['email']).first()
+        if user:
+            user.twitter_api_key = request.form.get("api_key")
+            user.twitter_api_secret = request.form.get("api_secret")
+            user.twitter_access_token = request.form.get("access_token")
+            user.twitter_access_token_secret = request.form.get("access_token_secret")
+            user.screen_name = request.form.get("screen_name")
+            user.client_id = request.form.get("client_id")
+            user.client_secret = request.form.get("client_secret")
+            user.isFilledApiDetails=True
+            user.canPost=True
+            db.session.commit()
+
+            # tweets = get_user_tweets()
+            # return redirect(url_for("dashboard"))
+            return render_template("dashboard.html" )
+        else:
+            session['canPost'] = False
+            return redirect(url_for("dashboard"))
+    if request.method == "GET":
+        # return render_template("get_api_details.html")
+        user=User.query.filter_by(email=session['email']).first()
+        if user:
+            if user.isFilledApiDetails:
+                redirect(url_for("get_api_details"))
+
+            else:
+                user.client_id = None
+                user.client_secret = None
+                user.twitter_access_token = None
+                user.twitter_access_token_secret = None
+                user.twitter_api_key = None
+                user.twitter_api_secret = None
+                user.screen_name = None
+                db.session.commit()
+                return redirect(url_for("get_api_details"))
+
+        else:
+            return redirect(url_for("login"))
+
+@app.route("/get_api_details")
+def get_api_details():
+    return render_template("get_api_details.html")
 
 
+@app.route("/show_api_credentials" )
+def show_api_credentials():
 
-
-
+        user=User.query.filter_by(email=session['email']).first()
+        if user:
+            client_id = user.client_id
+            client_secret = user.client_secret
+            user_access_token = user.twitter_access_token
+            user_access_token_secret = user.twitter_access_token_secret
+            twitter_api_key = user.twitter_api_key
+            twitter_api_secret = user.twitter_api_secret
+            if not client_id or not client_secret or not user_access_token or not user_access_token_secret or not twitter_api_key or not twitter_api_secret:
+                 return render_template("show_api_details.html",client_id=client_id,client_secret=client_secret,user_access_token=user_access_token,user_access_token_secret=user_access_token_secret,twitter_api_key=twitter_api_key,twitter_api_secret=twitter_api_secret)
+            else:
+                flash("Please fill the API details!")
+        else:
+            flash("Please login first!")
 #logout
-@app.route("/logout")
+@app.route("/logout",methods=["POST"])
 def logout():
-    session.pop("username",None)
-    session.pop("email",None)
-    return redirect(url_for("login"))
-
+    if request.method == "POST":
+        session.pop("username",None)
+        session.pop("email",None)
+        session.pop("toDashboard",False)
+        return redirect(url_for("login"))
 
 
 
@@ -235,4 +292,5 @@ def logout():
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
+
     app.run(debug=True)
--- test.py
+++ test.py
@@ -1,1 +1,44 @@
+import tweepy
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
+# Your client ID and secret should be stored securely
+client_id = os.getenv("Client_ID")
+client_secret = os.getenv("Client_Secret")
+user_access_token=os.getenv("TWITTER_USER_ACCESS_TOKEN")
+twitter_api_key=os.getenv("TWITTER_API_KEY")
+twitter_api_secret=os.getenv("TWITTER_API_SECRET")
+twitter_access_token=os.getenv("TWITTER_ACCESS_TOKEN")
+twitter_access_token_secret=os.getenv("TWITTER_ACCESS_TOKEN_SECRET")
+# This is the user's access token obtained from the authorization flow
+
+user_access_token_secret = os.getenv("TWITTER_USER_ACCESS_TOKEN_SECRET")
+
+def post_tweet():
+    """
+    Handles a form submission to post a new tweet using OAuth 1.0a User Context.
+    """
+    tweet_text =  "Hello from my Flask app!"
+    if not tweet_text:
+        return "Tweet content is required", 400
+
+
+
+    # Use the tweepy.Client with OAuth 1.0a User Context
+    client = tweepy.Client(
+        consumer_key= twitter_api_key,
+        consumer_secret= twitter_api_secret,
+        access_token= twitter_access_token,
+        access_token_secret= twitter_access_token_secret
+    )
+
+    try:
+        response = client.create_tweet(text=tweet_text)
+        print("✅ Tweet posted successfully:", response.data)
+        # Add success flash message here
+    except Exception as e:
+        print("❌ Error posting tweet:", e)
+        # Add error flash message here
+
+post_tweet()
